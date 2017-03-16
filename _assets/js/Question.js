22f674e1bb327b46ed796acf12eaa923b2c34f65
magicspon <leon@ournameismud.co.uk>
2017-03-16 01:26:03 +0000

    fixed

--- _assets/js/Question.js
+++ _assets/js/Question.js
@@ -8,25 +8,27 @@ const template =
 		<li>
 			<h2>{{ title }}</h2>
 			<div v-html="text"></div>
-			<div v-for="(answer, index) in answers">
-				<div v-if="type === 'checkbox'">
-					<label :for="id + index">
-						<input :value="answer.id" v-model="user" :name="id + index" :id="id + index" type="checkbox"/>
-						{{ answer.text }}
-					</label>
+			<form v-on:submit.prevent="onSubmit" ref="form">
+				<div v-for="(answer, index) in answers">
+					<div v-if="type === 'checkbox'">
+						<label :for="id + index">
+							<input :value="answer.id" v-model="user" :name="id + index" :id="id + index" type="checkbox"/>
+							{{ answer.text }}
+						</label>
+					</div>
+
+					<div v-if="type === 'radio'">
+						<label :for="id  + index">
+							<input :value="answer.id" v-model="user" :name="id" :id="id  + index" type="radio"/>
+							{{ answer.text }}
+						</label>
+					</div>
 				</div>
 
-				<div v-if="type === 'radio'">
-					<label :for="id  + index">
-						<input :value="answer.id" v-model="user" :name="id" :id="id  + index" type="radio"/>
-						{{ answer.text }}
-					</label>
-				</div>
-			</div>
+				<button @click.prevent="clickHandle">Answer</button>
+			</form>
 
-			<button @click.prevent="clickHandle">Answer</button>
-
-			<div v-if="feedback">
+			<div v-if="asked">
 				{{ feedback }}
 			</div>
 		</li>
@@ -76,9 +78,9 @@ export default {
 	data() {
 		return {
 			user: [],
-			output: [],
-			feedback: false,
-			correct: false
+			asked: false,
+			correct: false,
+			prev: null
 		}
 	},
 
@@ -89,6 +91,15 @@ export default {
 	computed: {
 		total() {
 			return this.answers.filter((answer) => answer.correct).length
+		},
+
+		correctedIds() {
+			return this.answers.filter((answer) => answer.correct).map((answer) => answer.id)
+		},
+
+		feedback() {
+			const { correct, incorrect } = this.responses
+			return this.correct ? correct : incorrect
 		}
 	},
 
@@ -102,40 +113,45 @@ export default {
 			input is the :value from the input
 		*/
 		user(input) {
-			// run away if there ain't any shiz
-			if(input.length < 1) return
-			let response
 
-			// if its a string... we want to find the answer with a matching string and a correct property
-			if(typeof input === 'string') {
-				// using destructuring here to get the id/correct values from the array item
-				// find returns a value (not an array)
-				response = this.answers.find(({id, correct}) => id === input && correct)
+			if(!this.correct) {
+				if(this.type === 'checkbox') {
+					if(input.filter((id) => this.correctedIds.includes(id)).length === this.total && input.length <= this.total) {
+						this.correct = true
+					}
+				} 
+
+				if(this.type === 'radio') {
+					if(this.correctedIds.includes(input)) {
+						this.correct = true
+						log('correct')
+					}
+				} 
+
 			} else {
-				// if it's not a string, i've presumed it's gonna be an array
-				// reducing over the array, returning true once a match is found 
-				// reduce is super funking awesome
-				response = input.reduce((acc, curr) => this.answers.find(({id, correct}) => id === curr && correct) || acc, '')
+				this.correct = false
 			}
 
-			// once we have a trufy response, punch it into the output array
-			if(response) {
-				this.output.push(response)
+			if(this.feedback) {
+				this.feedback 
 			}
-
-			// once the output array is the same length as the total number of correct answers
-			if(this.output.length === this.total) {
-				this.correct = true
+			
+			if(input.length === 0) {
+				this.asked = false
 			}
 		}
 	},
 
+
 	methods: {
 		clickHandle() {
-			// is it correct... update the feedback value
-			const { correct, incorrect } = this.responses
-			this.feedback = this.correct ? correct : incorrect
-		}
+			this.asked = true
+		},
+
+		onSubmit() {
+			this.asked = true
+		},
+		
 	},
 
 	template
