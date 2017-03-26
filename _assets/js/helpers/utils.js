f8bca71799fa05368bf181eea887bfeb2f217679
magicspon <leon@ournameismud.co.uk>
2017-03-27 00:47:52 +0100

    jsbin link for fancy function thinger

--- _assets/js/helpers/utils.js
+++ _assets/js/helpers/utils.js
@@ -1,4 +1,5 @@
 // keen
+// http://jsbin.com/nenepizoze/edit?js,console,output
 export function insertAtIndex(item, index, array) {
 	return [...array.slice(0, index), item, ...array.slice(index + 1, array.length)]
 }
